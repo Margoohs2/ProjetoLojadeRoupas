2c93052576f31c69c72678a04f0b6619f0142e6b
Margoohs2 <margaridaunic@gmail.com>
2024-10-12 17:14:20 -0300

    Report

--- SistemaLojadeRoupas/backend/src/main/java/br/edu/unichristus/data/model/Report.java
+++ SistemaLojadeRoupas/backend/src/main/java/br/edu/unichristus/data/model/Report.java
@@ -1,4 +1,17 @@
 package br.edu.unichristus.data.model;
-
+import jakarta.persistence.*;
+import lombok.Data;
+@Entity
+@Table(name = "tb_report")
+@Data
 public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private int quantity;
+    private double totalvalue;
 }
