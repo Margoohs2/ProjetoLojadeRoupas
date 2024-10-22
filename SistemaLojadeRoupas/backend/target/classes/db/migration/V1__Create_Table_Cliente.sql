ddb520aea41f1ece41d38769280e5aa22debfdc8
Margoohs2 <margaridaunic@gmail.com>
2024-10-21 21:11:59 -0300

    Correção migration

--- SistemaLojadeRoupas/backend/target/classes/db/migration/V1__Create_Table_Cliente.sql
+++ SistemaLojadeRoupas/backend/target/classes/db/migration/V1__Create_Table_Cliente.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cliente (
-	id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL,
-	email TEXT,
-	phone TEXT,
-	password TEXT
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email VARCHAR(255) UNIQUE,
+    phone VARCHAR(20),
+    password VARCHAR(255)
 );
