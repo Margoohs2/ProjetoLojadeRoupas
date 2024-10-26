887a9830c1250f15efa19a076c57af97a7db6d83
AnnaPaula07 <126425985+AnnaPaula07@users.noreply.github.com>
2024-10-25 21:03:11 -0300

    Banco de dados

--- SistemaLojadeRoupas/backend/src/main/resources/db/migration/V1__Create_Table_Client.sql
+++ SistemaLojadeRoupas/backend/src/main/resources/db/migration/V1__Create_Table_Client.sql
@@ -1,7 +1,7 @@
-CREATE TABLE client (
-	id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL,
-	email TEXT,
-	phone TEXT,
-	password TEXT
+CREATE TABLE public.tb_client (
+    id bigint NOT NULL,
+    email character varying(150) NOT NULL,
+    name character varying(150) NOT NULL,
+    password character varying(255) NOT NULL,
+    phone character varying(20) NOT NULL
 );
