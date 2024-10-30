f41a073d75a16ad483e7481a7ae169aba7531196
AnnaPaula07 <126425985+AnnaPaula07@users.noreply.github.com>
2024-10-29 22:08:55 -0300

    CRUDs no salecontroller

--- SistemaLojadeRoupas/backend/src/main/java/br/edu/unichristus/controller/SaleController.java
+++ SistemaLojadeRoupas/backend/src/main/java/br/edu/unichristus/controller/SaleController.java
@@ -1,14 +1,14 @@
 package br.edu.unichristus.controller;
 
-import br.edu.unichristus.data.model.Product;
+import br.edu.unichristus.data.dto.ProductDTO;
+import br.edu.unichristus.data.dto.SaleDTO;
+import br.edu.unichristus.data.model.Sale;
 import br.edu.unichristus.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/reports")
@@ -16,6 +16,26 @@ public class SaleController {
 
     @Autowired
     private SaleService service;
+
+    @PostMapping
+    public SaleDTO create(@RequestBody SaleDTO sale){
+        return service.save(sale);
+    }
+
+    @PutMapping
+    public SaleDTO update(@RequestBody SaleDTO sale){
+        return service.save(sale);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        service.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public SaleDTO findById(@PathVariable Long id){
+        return service.findById(id);
+    }
 
     @GetMapping("/sales")
     public ResponseEntity<String> gerarRelatorio() {
