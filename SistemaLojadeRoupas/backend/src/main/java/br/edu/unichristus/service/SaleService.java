220bb9f70d106f9c38475257c8f881c482c0ab6b
AnnaPaula07 <126425985+AnnaPaula07@users.noreply.github.com>
2024-10-29 22:03:10 -0300

    CRUDs no saleservice

--- SistemaLojadeRoupas/backend/src/main/java/br/edu/unichristus/service/SaleService.java
+++ SistemaLojadeRoupas/backend/src/main/java/br/edu/unichristus/service/SaleService.java
@@ -1,21 +1,41 @@
 package br.edu.unichristus.service;
 
 import br.edu.unichristus.data.dto.SaleDTO;
-import br.edu.unichristus.data.model.Product;
 import br.edu.unichristus.data.model.Sale;
-import br.edu.unichristus.repository.SaleRepository;
+import br.edu.unichristus.dozer.DozerConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class SaleService {
 
     @Autowired
     private br.edu.unichristus.repository.SaleRepository repository;
+
+    public SaleDTO save(SaleDTO sale){
+        var entity = DozerConverter.parseObject(sale, Sale.class);
+
+        var entityDTO = repository.save(entity);
+
+        return DozerConverter.parseObject(entityDTO, SaleDTO.class);
+    }
+
+    public List<SaleDTO> findAll(){
+        return DozerConverter.parseListObjects(
+                repository.findAll(), SaleDTO.class);
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
+    public SaleDTO findById(Long id){
+        var entity = repository.findById(id);
+
+        return DozerConverter.parseObject(entity.get(), SaleDTO.class);
+    }
 
     public String generateReport() {
         // Buscar todas as vendas salvas no banco de dados
