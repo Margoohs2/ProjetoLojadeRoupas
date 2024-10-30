2c0f25bb9db32b6a53480540186fe29812a5c452
AnnaPaula07 <126425985+AnnaPaula07@users.noreply.github.com>
2024-10-29 21:25:19 -0300

    Ajustes no SaleDTO

--- SistemaLojadeRoupas/backend/src/main/java/br/edu/unichristus/data/dto/SaleDTO.java
+++ SistemaLojadeRoupas/backend/src/main/java/br/edu/unichristus/data/dto/SaleDTO.java
@@ -1,9 +1,12 @@
 package br.edu.unichristus.data.dto;
 
+import br.edu.unichristus.data.model.Client;
+import br.edu.unichristus.data.model.Product;
 import lombok.Data;
 @Data
 public class SaleDTO {
-    private String itemName;
-    private int quantitySold;
-    private double totalValue;
+    private Client client;
+    private Product product;
+    private int quantity;
+    private double totalvalue;
 }
