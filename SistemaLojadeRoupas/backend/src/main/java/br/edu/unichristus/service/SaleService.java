a81a7769c67ec314045d2e06127dacb776a90cd9
AnnaPaula07 <126425985+AnnaPaula07@users.noreply.github.com>
2024-10-29 21:29:11 -0300

    Ajuste no SaleService

--- SistemaLojadeRoupas/backend/src/main/java/br/edu/unichristus/service/SaleService.java
+++ SistemaLojadeRoupas/backend/src/main/java/br/edu/unichristus/service/SaleService.java
@@ -16,20 +16,4 @@ public class SaleService {
     @Autowired
     private br.edu.unichristus.repository.SaleRepository repository;
 
-    public List<SaleDTO> generateSalesReport() {
-
-        List<Sale> productReports = repository.findAll();
-
-        return productReports.stream().map(productReport -> {
-            SaleDTO dto = new SaleDTO();
-            dto.setItemName(productReport.getProduct().getName());
-            dto.setQuantitySold(productReport.getQuantity());
-            dto.setTotalValue(productReport.getQuantity() * productReport.getProduct().getPrice());
-            return dto;
-        }).collect(Collectors.toList());
-    }
-
-    public Map<Product, Integer> generateProductReport() {
-        return Map.of();
-    }
 }
